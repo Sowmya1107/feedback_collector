b2fc7eb550483882d8af8776a65156d61a8faff1
Sowmya Krishna <st189939@stud.uni-stuttgart.de>
2025-07-16 19:24:10 +0200

    changes to app

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,18 @@
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from datetime import datetime
-from feedback_store import feedback_store
 import os
+
+# In-memory storage for feedbacks
+feedback_store = []
 
 # Adjust path so Flask knows where the HTML is
 app = Flask(__name__, static_folder='../frontend', static_url_path='/')
 CORS(app)
-from flask import send_from_directory
+
 @app.route('/')
 def serve_html():
     return send_from_directory('../frontend', 'index.html')
-
-#@app.route('/')
-#def serve_home():
- #   return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/feedback', methods=['POST'])
 def submit_feedback():
@@ -22,16 +20,14 @@ def submit_feedback():
     feedback = {
         "text": data["text"][:250],
         "rating": int(data["rating"]),
-        "timestamp": datetime.now().isoformat()  # Use server's local time
+        "timestamp": datetime.now().isoformat()
     }
     feedback_store.append(feedback)
     return jsonify({"message": "Feedback added"}), 201
 
-    
 @app.route('/feedback', methods=['GET'])
 def get_feedback():
     return jsonify(feedback_store), 200
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
